faff47c9781a806bef584109f8127951c169eadb
Malavé <luis.malave@gmail.com>
2021-03-15 17:49:59 +0100

    Adding version numbers to helm files Upgrade to 0.0.6 (second try)

--- scripts/helm_upgrade.sh
+++ scripts/helm_upgrade.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #helm install -v 9 --create-namespace=true --set-string instanceName=$1 $1 ../charts/articulate/
 #helm upgrade -v 9 scovey ../charts/scovey --install --wait --atomic --namespace=scovey --set=app.name=scovey --values=../charts/scovey/values.yaml -f values.yaml
-helm upgrade -v 9 springservice ../servicechart/ --install --wait --debug --namespace=springservice --values=../servicechart/values.yaml
+helm upgrade -v 9 springservice ../servicechart/ --install --wait --debug --namespace=springservice --set=app.version=0.0.6 --values=../servicechart/values.yaml
