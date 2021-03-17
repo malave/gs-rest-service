a64f409d39ec17ed97f1d3ba3e2cf00c0f6d84c5
Malavé <luis.malave@gmail.com>
2021-03-17 15:33:43 +0100

    Reverted to hello world

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -12,8 +12,8 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private static final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "Other") String name) {
+	@GetMapping("/")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
