a68e8ffeb21d3908472bbd2a97cd38bfcb2f621c
Luis Malave <luis.malave@gmail.com>
2021-03-18 21:20:51 +0100

    Presentacion (#7)
    * Upgrade to 1.0.0
    
    * Cambio de mensaje
    
    * Cambio de mensaje
    
    * Cambio de mensaje
    
    * Cambio de mensaje

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "Hola, %s!";
 	private static final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "Mundo") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
