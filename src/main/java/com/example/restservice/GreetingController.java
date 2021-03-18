01c7f59c32e20d30b61ec99aaf13f1187f201e80
Malavé <luis.malave@gmail.com>
2021-03-18 19:46:44 +0100

    Cambio de mensaje

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
 	private static final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "MDAS") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
